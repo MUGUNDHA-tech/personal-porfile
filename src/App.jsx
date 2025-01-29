96050f4677b5fbe835bde9c52d1d18f85961694e
MUGUNDHA-tech <mugub06@gmail.com>
2025-01-29 15:09:33 +0530

    profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import backImage from './assets/images/back.jpg';
+import backImage from './assets/images/Back.jpg';
 import './App.css';
 
 function Navbar() {
